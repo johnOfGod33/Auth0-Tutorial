b4a7b9c273bedfebae8abb4299c19da3b2f96433
jean de dieu <jeandedieusessou@gmail.com>
2025-01-27 11:57:30 +0000

    setup environment variables and config OPEN ID router

--- README.md
+++ README.md
@@ -15,3 +15,9 @@ Auth0 is a third-part solution to add authentication and authorization services 
 ```bash
 npm install express express-openid-connect --save
 ```
+
+### Configure callback URL and logout url
+
+![img](/static/assets/Screenshot%20from%202025-01-27%2011-43-51.png)
+
+save and continue
--- server.js
+++ server.js
@@ -1,15 +1,30 @@
 const express = require("express");
-const path = require("path");
 const app = express();
+const { auth } = require("express-openid-connect");
+const dotenv = require("dotenv");
 const port = process.env.PORT || 3000;
+
+dotenv.config();
+
+const { BASE_URL, CLIENT_ID, ISSUER_BASE_URL, SECRET } = process.env;
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  baseURL: `${BASE_URL}/${port}`,
+  clientID: CLIENT_ID,
+  issuerBaseURL: ISSUER_BASE_URL,
+  secret: SECRET,
+};
 
 app.set("view engine", "ejs");
 
 app.use(express.static("public"));
 app.use(express.json());
+app.use(auth(config));
 
 app.get("/", (req, res) => {
-  const isAuthenticated = false;
+  const isAuthenticated = req.oidc.isAuthenticated();
   const user = isAuthenticated
     ? { name: "Jean de Dieu", email: "jeandieu@example.com" }
     : null;
